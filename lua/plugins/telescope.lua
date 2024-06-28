ffab74e93406a02825ceb7e6953d51ad3ed47435
Ash Gent <ash_gent@sweetwater.com>
2024-06-28 08:49:31 -0400

    Update telescope layout

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,12 +10,12 @@ return {
     },
   },
   -- change some options
-  -- opts = {
-  --   defaults = {
-  --     layout_strategy = "horizontal",
-  --     layout_config = { prompt_position = "top" },
-  --     sorting_strategy = "ascending",
-  --     winblend = 0,
-  --   },
-  -- },
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+    },
+  },
 }
