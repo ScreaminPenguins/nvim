59329b23897d91ba6a9e0f71c011ab5e6d491139
Ash Gent <ash_gent@sweetwater.com>
2024-07-08 08:01:28 -0400

    Update telescope ordering

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -114,6 +114,7 @@ return {
       defaults = {
         prompt_prefix = " ",
         selection_caret = " ",
+        sorting_strategy = "ascending",
         layout_strategy = "horizontal",
         layout_config = {
           prompt_position = "top",
