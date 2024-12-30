abd7f36427ead92c578e136b849c40125b081264
Ash Gent <ash_gent@sweetwater.com>
2024-12-30 08:55:53 -0500

    WIP

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
 		}),
 		config = function()
 			vim.keymap.set("n", "<leader>fd", require("telescope.builtin").find_files)
+			vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
 			vim.keymap.set("n", "<leader>en", function()
 				require("telescope.builtin").find_files({
 					cwd = vim.fn.stdpath("config")
