fdbb3df531c2f9051a8a071bf59c97af9f41499b
Ash Gent <ash_gent@sweetwater.com>
2024-12-30 08:02:00 -0500

    WIP

---
+++ lua/custom_plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato",
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
