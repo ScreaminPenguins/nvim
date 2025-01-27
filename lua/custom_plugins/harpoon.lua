96a5b8835211a048841e6d3ed1fb092b8868b63b
Ash Gent <ash_gent@sweetwater.com>
2025-01-27 08:14:02 -0500

    wip

--- lua/custom_plugins/harpoon.lua
+++ lua/custom_plugins/harpoon.lua
@@ -1,60 +1,62 @@
 return {
-  "theprimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("harpoon"):setup()
-  end,
-  keys = {
-    {
-      "<leader>h",
-      function()
-        local harpoon = require("harpoon")
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = "Harpoon Quick Menu",
-    },
-    {
-      "<leader>H",
-      function()
-        require("harpoon"):list():add()
-      end,
-      desc = "Harpoon File",
-    },
-    {
-      "<leader>1",
-      function()
-        require("harpoon"):list():select(1)
-      end,
-      desc = "Harpoon File GoTo: 1",
-    },
-    {
-      "<leader>2",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      desc = "Harpoon File GoTo: 2",
-    },
-    {
-      "<leader>3",
-      function()
-        require("harpoon"):list():select(3)
-      end,
-      desc = "Harpoon File GoTo: 3",
-    },
-    {
-      "<leader>4",
-      function()
-        require("harpoon"):list():select(4)
-      end,
-      desc = "Harpoon File GoTo: 4",
-    },
-    {
-      "<leader>5",
-      function()
-        require("harpoon"):list():select(5)
-      end,
-      desc = "Harpoon File GoTo: 5",
+  {
+    "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end,
+    keys = {
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+      {
+        "<leader>H",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Harpoon File",
+      },
+      {
+        "<leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "Harpoon File GoTo: 1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "Harpoon File GoTo: 2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "Harpoon File GoTo: 3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "Harpoon File GoTo: 4",
+      },
+      {
+        "<leader>5",
+        function()
+          require("harpoon"):list():select(5)
+        end,
+        desc = "Harpoon File GoTo: 5",
+      },
     },
   },
 }
---
+++ lua/custom_plugins/kanagawa.lua
@@ -0,0 +1,10 @@
+return {
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
