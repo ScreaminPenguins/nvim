cc27e6be79eb640da0dd1ecc9ec7e56215520f27
Ash Gent <ash_gent@sweetwater.com>
2025-02-04 16:28:01 -0500

    wip

--- lua/custom_plugins/harpoon.lua
+++ lua/custom_plugins/harpoon.lua
@@ -4,7 +4,7 @@ return {
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("harpoon"):setup()
+      require("harpoon"):setup({})
     end,
     keys = {
       {
