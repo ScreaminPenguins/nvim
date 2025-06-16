f627b28bb292702021076ca66c412831bc49f8a6
Ash Gent <ash_gent@sweetwater.com>
2025-06-16 08:50:02 -0400

    add more defaults to init

--- init.lua
+++ init.lua
@@ -9,3 +9,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":.lua<CR>")
