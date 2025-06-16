65a1b78b7d0e7625987674f000b5c9eda03b12cb
Ash Gent <ash_gent@sweetwater.com>
2025-06-16 07:53:36 -0400

    Add highlight on yank

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Highlight on text yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highligh on text yank",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
