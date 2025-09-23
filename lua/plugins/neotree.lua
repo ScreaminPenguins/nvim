ad8c2c5b0445c8fd36371bb2fa15158770e8278c
Ash Gent <ash_gent@sweetwater.com>
2025-09-23 08:32:53 -0400

    wip

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,32 +1,32 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons", -- optional, but recommended
-		},
-		lazy = false,                 -- neo-tree will lazily load itself
-		---@module 'neo-tree'
-		---@type neotree.Config
-		opts = {
-			filesystem = {
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					hide_by_name = {}
-				},
-			},
-		},
-		keys = {
-			{
-				"<leader>E",
-				function()
-					vim.cmd("Neotree toggle")
-				end,
-				desc = "Neotree: Toggle",
-			},
-		},
-	},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module 'neo-tree'
+    ---@type neotree.Config
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {},
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>E",
+        function()
+          vim.cmd("Neotree toggle")
+        end,
+        desc = "Neotree: Toggle",
+      },
+    },
+  },
 }
