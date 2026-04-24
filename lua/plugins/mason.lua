c379606c010fb0898a2b57e2533bb8ba8324a8e8
Ash Gent <ash_gent@sweetwater.com>
2026-04-24 11:22:16 -0400

    Update plugin/mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,225 +1,52 @@
-return {
-	{
-		"mason-org/mason.nvim",
-		build = ":MasonUpdate",
-		opts = {
-			ensure_installed = {
-				"ty", -- Testing this vs basedpyright
-				-- "basedpyright",
-				"bash-language-server",
-				"debugpy",
-				"docker-language-server",
-				"gopls",
-				"helm-ls",
-				"lua-language-server",
-				"ruff",
-				"stylua",
-				"yaml-language-server",
-				"prettier",
-			},
-		},
-		opts_extend = { "ensure_installed" },
+-- mason.nvim — LSP server, formatter, and tool installer
+-- https://github.com/mason-org/mason.nvim
 
-		---@param opts MasonSettings | {ensure_installed: string[]}
-		config = function(_, opts)
-			require("mason").setup(opts)
-			local mr = require("mason-registry")
-			mr:on("package:install:success", function()
-				vim.defer_fn(function()
-					require("lazy.core.handler.event").trigger({
-						event = "FileType",
-						buf = vim.api.nvim_get_current_buf(),
-					})
-				end, 100)
-			end)
+local ensure_installed = {
+  -- Language servers
+  "ty",                    -- Python (replacing basedpyright)
+  "bash-language-server",  -- Shell
+  "buf",                   -- Protobuf (lspconfig server name: buf_ls)
+  "docker-language-server", -- Docker
+  "gopls",                 -- GoLang
+  "helm-ls",               -- Helm
+  "intelephense",          -- PHP
+  "jdtls",                 -- Java
+  "lua-language-server",   -- Lua
+  "pbls",                  -- Protobuf
+  "yaml-language-server",  -- Yaml
 
-			mr.refresh(function()
-				for _, tool in ipairs(opts.ensure_installed) do
-					local p = mr.get_package(tool)
-					if not p:is_installed() then
-						p:install()
-					end
-				end
-			end)
+  -- Linters / formatters
+  "ruff",    -- Python
+  "stylua",  -- Lua
+  "prettier", -- Misc
 
-			vim.keymap.set("n", "<leader>tM", "<cmd>Mason<cr>", { desc = "Toggle Mason UI" })
-		end,
-
-		keys = {},
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		dependencies = {
-			"mason.nvim",
-		},
-		cmd = { "DapInstall", "DapUninstall" },
-		config = function() end,
-	},
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"mfussenegger/nvim-dap-python",
-			{
-				-- virtual text for debugger
-				"theHamsta/nvim-dap-virtual-text",
-				opts = {},
-			},
-		},
-
-		keys = {
-			{
-				"<leader>dB",
-				function()
-					require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-				end,
-				desc = "Breakpoint Condition",
-			},
-			{
-				"<leader>db",
-				function()
-					require("dap").toggle_breakpoint()
-				end,
-				desc = "Toggle Breakpoint",
-			},
-			{
-				"<leader>dc",
-				function()
-					require("dap").continue()
-				end,
-				desc = "Run/Continue",
-			},
-			{
-				"<leader>da",
-				function()
-					require("dap").continue({ before = get_args })
-				end,
-				desc = "Run with Args",
-			},
-			{
-				"<leader>dC",
-				function()
-					require("dap").run_to_cursor()
-				end,
-				desc = "Run to Cursor",
-			},
-			{
-				"<leader>dg",
-				function()
-					require("dap").goto_()
-				end,
-				desc = "Go to Line (No Execute)",
-			},
-			{
-				"<leader>di",
-				function()
-					require("dap").step_into()
-				end,
-				desc = "Step Into",
-			},
-			{
-				"<leader>dj",
-				function()
-					require("dap").down()
-				end,
-				desc = "Down",
-			},
-			{
-				"<leader>dk",
-				function()
-					require("dap").up()
-				end,
-				desc = "Up",
-			},
-			{
-				"<leader>dl",
-				function()
-					require("dap").run_last()
-				end,
-				desc = "Run Last",
-			},
-			{
-				"<leader>do",
-				function()
-					require("dap").step_out()
-				end,
-				desc = "Step Out",
-			},
-			{
-				"<leader>dO",
-				function()
-					require("dap").step_over()
-				end,
-				desc = "Step Over",
-			},
-			{
-				"<leader>dP",
-				function()
-					require("dap").pause()
-				end,
-				desc = "Pause",
-			},
-			{
-				"<leader>dr",
-				function()
-					require("dap").repl.toggle()
-				end,
-				desc = "Toggle REPL",
-			},
-			{
-				"<leader>ds",
-				function()
-					require("dap").session()
-				end,
-				desc = "Session",
-			},
-			{
-				"<leader>dt",
-				function()
-					require("dap").terminate()
-				end,
-				desc = "Terminate",
-			},
-			{
-				"<leader>dw",
-				function()
-					require("dap.ui.widgets").hover()
-				end,
-				desc = "Widgets",
-			},
-		},
-		config = function()
-			require("mason-nvim-dap").setup()
-		end,
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = { "nvim-neotest/nvim-nio" },
-    -- stylua: ignore
-    keys = {
-      { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
-      { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
-    },
-		opts = {},
-		config = function(_, opts)
-			local dap = require("dap")
-			local dapui = require("dapui")
-			dapui.setup(opts)
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open({})
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close({})
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close({})
-			end
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap-python",
-		config = function()
-			require("dap-python").setup("uv")
-		end,
-	},
+  -- Debuggers
+  "debugpy", -- Python
 }
+
+require("mason").setup()
+
+local mr = require("mason-registry")
+
+-- When a package finishes installing mid-session, re-trigger FileType so LSP
+-- handlers re-evaluate the current buffer and attach the new server.
+-- Replaces the lazy.nvim internal previously used here.
+mr:on("package:install:success", function()
+  vim.defer_fn(function()
+    vim.api.nvim_exec_autocmds("FileType", {
+      buf = vim.api.nvim_get_current_buf(),
+    })
+  end, 100)
+end)
+
+-- Install any missing tools on startup
+mr.refresh(function()
+  for _, tool in ipairs(ensure_installed) do
+    local p = mr.get_package(tool)
+    if not p:is_installed() then
+      p:install()
+    end
+  end
+end)
+
+vim.keymap.set("n", "<leader>tM", "<cmd>Mason<cr>", { desc = "Mason" })
