e8c4dba64cad0cad9498f99419e9241e68a1d704
Ash Gent <ash_gent@sweetwater.com>
2026-04-24 10:18:46 -0400

    Align vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,16 +1,12 @@
 local opt = vim.opt
 
-opt.foldlevel = 99
-opt.cmdheight = 0
 opt.clipboard = "unnamedplus"
+opt.cmdheight = 0
 opt.confirm = true
 opt.expandtab = true
-opt.number = true
-opt.relativenumber = true
-opt.shiftwidth = 4
-opt.splitbelow = true
-opt.tabstop = 4
-opt.wrap = false
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldlevel = 99
+opt.foldmethod = "expr"
 opt.list = false
 opt.listchars = {
 	tab = "» ",
@@ -18,3 +14,15 @@ opt.listchars = {
 	space = "·",
 	multispace = "---+",
 }
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 4
+opt.splitbelow = true
+opt.tabstop = 4
+opt.wrap = false
+
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "",
+	},
+})
